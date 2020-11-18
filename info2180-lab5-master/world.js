83e7067c88eecc4474d0c6b76be95d79197da293
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-11-17 23:04:37 -0500

    Finished exercise 5: time to style

--- info2180-lab5-master/world.js
+++ info2180-lab5-master/world.js
@@ -1,10 +1,26 @@
 window.onload = () => {
     var country = document.getElementById("country")
     var button = document.getElementById("lookup")
+    var context = document.getElementById("context")
     button.addEventListener('click', () => {
         var sanitizedSearchValue =  country.value.trim()
         this.fetch('http://localhost:8080/info2180-lab5/info2180-lab5-master/world.php?' + new URLSearchParams({
-            country: sanitizedSearchValue
+            country: sanitizedSearchValue,
+            context: "none"
+        }))
+        .then(response => response.text())
+        .then(data => {
+            this.document.getElementById("result").innerHTML = data;
+        })
+        .catch(error => {
+            this.alert(error);
+        });
+    })
+    context.addEventListener('click', () => {
+        var sanitizedSearchValue =  country.value.trim()
+        this.fetch('http://localhost:8080/info2180-lab5/info2180-lab5-master/world.php?' + new URLSearchParams({
+            country: sanitizedSearchValue,
+            context: "cities"
         }))
         .then(response => response.text())
         .then(data => {
--- info2180-lab5-master/world.php
+++ info2180-lab5-master/world.php
@@ -10,7 +10,9 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $value = $_GET["country"];
+$context = $_GET["context"];
 ?>
+<?php if($context == "none"){?>
 <table>
 <tr>
   <th>Name</th>
@@ -37,3 +39,29 @@ if(empty($value)){
   <tr><td><?= $row['name']?></td><td><?=$row['continent']?></td><td><?=$row['independence_year']?></td><td><?=$row['head_of_state']; ?></td></tr>
 <?php endforeach; }?>
 </table>
+<?php } elseif($context == "cities"){
+if(empty($value)){
+  echo("<p><strong>Please enter a country in the search bar.</strong></p>");
+} else {?>
+<table>
+<tr>
+  <th>Name</th>
+  <th>District</th>
+  <th>Population</th>
+</tr>
+<?php
+  $stmt1 = $conn->prepare("SELECT code FROM countries WHERE `name` LIKE ?");
+  $stmt1->execute(array("%$value%"));
+  $country_codes = $stmt1->fetchAll();
+
+  $stmt2 = $conn->prepare("SELECT name, district, population FROM cities WHERE `country_code` LIKE ?");
+  foreach ($country_codes as $row):
+    $stmt2->execute(array("%".$row["code"]."%"));
+    $result = $stmt2->fetchAll();?>
+    <?php foreach ($result as $row): ?>
+      <tr><td><?= $row['name']?></td><td><?=$row['district']?></td><td><?=$row['population']?></td></tr>
+    <?php endforeach; ?>
+<?php endforeach; }?>
+</table>
+
+<?php }?>
