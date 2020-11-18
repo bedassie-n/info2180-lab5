3466ff6a18227054237054479522a159affbd3e2
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-11-18 00:01:54 -0500

    Finished Lab 5

--- world.js
+++ world.js
@@ -4,7 +4,7 @@ window.onload = () => {
     var context = document.getElementById("context")
     button.addEventListener('click', () => {
         var sanitizedSearchValue =  country.value.trim()
-        this.fetch('http://localhost:8080/info2180-lab5/info2180-lab5-master/world.php?' + new URLSearchParams({
+        this.fetch('http://localhost:8080/info2180-lab5/world.php?' + new URLSearchParams({
             country: sanitizedSearchValue,
             context: "none"
         }))
